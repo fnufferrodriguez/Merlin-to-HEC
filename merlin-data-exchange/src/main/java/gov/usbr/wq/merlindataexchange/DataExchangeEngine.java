862718fd0f901a515bfa33e69ac4e191b8a83ecf
Bryson Spilman <bryson@rmanet.com>
2023-02-21 14:01:28 -0800

    Added javadoc for runExtract

--- merlin-data-exchange/src/main/java/gov/usbr/wq/merlindataexchange/DataExchangeEngine.java
+++ merlin-data-exchange/src/main/java/gov/usbr/wq/merlindataexchange/DataExchangeEngine.java
@@ -4,6 +4,12 @@ import java.util.concurrent.CompletableFuture;
 
 public interface DataExchangeEngine
 {
+    /**
+     *
+     * @return CompletableFuture containing status of extract.
+     * Calling runExtract() will block any threads if an extract is already running for this DataExchangeEngine instance
+     * i.e. If you want to run extracts in parallel, multiple DataExchangeEngines must be built
+     */
     CompletableFuture<MerlinDataExchangeStatus> runExtract();
 
     void cancelExtract();
