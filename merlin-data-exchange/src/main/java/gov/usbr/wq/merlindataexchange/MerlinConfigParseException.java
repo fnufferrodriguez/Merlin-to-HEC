8fbdf96b4686a46a488226d18d1ccfd8d96f5ff0
Bryson Spilman <bryson@rmanet.com>
2023-03-08 13:51:22 -0800

    Updated expected fields to only show the localName fields.

--- merlin-data-exchange/src/main/java/gov/usbr/wq/merlindataexchange/MerlinConfigParseException.java
+++ merlin-data-exchange/src/main/java/gov/usbr/wq/merlindataexchange/MerlinConfigParseException.java
@@ -3,7 +3,13 @@ package gov.usbr.wq.merlindataexchange;
 import com.fasterxml.jackson.databind.exc.UnrecognizedPropertyException;
 import org.xml.sax.SAXParseException;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 public final class MerlinConfigParseException extends Exception
 {
@@ -23,7 +29,7 @@ public final class MerlinConfigParseException extends Exception
         if(e instanceof UnrecognizedPropertyException)
         {
             retVal += ":\n" + ((UnrecognizedPropertyException)e).getOriginalMessage()
-                    + ".\nExpected one of the following fields: " +((UnrecognizedPropertyException) e).getKnownPropertyIds();
+                    + ".\nExpected one of the following fields: " + getExpectedFields((UnrecognizedPropertyException) e);
         }
         if(e instanceof SAXParseException)
         {
@@ -37,4 +43,41 @@ public final class MerlinConfigParseException extends Exception
         }
         return retVal;
     }
+
+    private static String getExpectedFields(UnrecognizedPropertyException e)
+    {
+        List<String> fieldNames = Arrays.stream(e.getReferringClass().getDeclaredFields())
+                .map(MerlinConfigParseException::getFieldAsPropertyName)
+                .collect(toList());
+        List<String> methodNames = Arrays.stream(e.getReferringClass().getDeclaredMethods())
+                .map(MerlinConfigParseException::getMethodAsPropertyName)
+                .collect(toList());
+        return e.getKnownPropertyIds().stream()
+                .filter(id -> !fieldNames.contains(id.toString()) && !methodNames.contains(id.toString()))
+                .collect(toList()).toString();
+    }
+
+    private static String getMethodAsPropertyName(Method method)
+    {
+        String retVal = method.getName();
+        if(retVal.startsWith("get") || retVal.startsWith("set"))
+        {
+            retVal = retVal.substring(3);
+            char[] chars = retVal.toCharArray();
+            chars[0] = Character.toLowerCase(chars[0]);
+            retVal = new String(chars);
+        }
+        return retVal;
+    }
+
+    private static String getFieldAsPropertyName(Field f)
+    {
+        String retVal = f.getName();
+        if(retVal.startsWith("_"))
+        {
+            retVal = retVal.substring(1);
+        }
+        return retVal;
+    }
+
 }
